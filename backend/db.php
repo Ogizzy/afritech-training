39cf0b9fb2a1fc5851f7a7d5342098d8735fa964
Terhide Tyavyar Jnr. <iamogizzy@gmail.com>
2026-06-27 18:27:25 +0100

    update

--- backend/db.php
+++ backend/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db   = 'afdic_training';
 $user = 'afdic_training';
-$pass = '';
+$pass = 'secureAfritech100%';
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
   die(json_encode(['success'=>false,'message'=>'Database connection failed']));
